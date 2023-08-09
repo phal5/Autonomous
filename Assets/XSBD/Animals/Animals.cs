b77961dca5f84c7900a12313ffe9a1d296d8ab24
phal5 <77428425+phal5@users.noreply.github.com>
2023-08-09 16:14:04 +0900

    Updated AnimalMasterManager

--- Assets/XSBD/Animals/Animals.cs
+++ Assets/XSBD/Animals/Animals.cs
@@ -27,10 +27,28 @@ public class Animals : MonoBehaviour
             }
         }
     }
+    class Drop
+    {
+        [SerializeField] GameObject _prefab;
+        [SerializeField] Transform _parent;
+        [SerializeField] float _probability;
+
+        public void InstantiateDrop(Vector3 position)
+        {
+            if(Random.value < _probability)
+            {
+                Instantiate(_prefab, position, Random.rotation, _parent);
+            }
+        }
+    }
 
     [Header("NavMesh and Speed")]
     [SerializeField] NavMeshAgent _agent;
+    float _originalSpeed; // 기존 동물 속도 저장용
+
     [SerializeField] Gimmick[] _gimmicks;
+
+    [Header("Perception Variables")]
     [SerializeField] Transform _mouth;
     [SerializeField] float _cognitiveDistance;
     [SerializeField] [Tooltip("small: 0 ~ big: 3, cow is 2")] byte _levelOfSize;
@@ -42,19 +60,18 @@ public class Animals : MonoBehaviour
     //HpManager Values
     float _originalHP;
     [SerializeField] float _animalHP;
+    [SerializeField] Drop[] _dropsWhenDead;
 
     //HungerManager Values
     [SerializeField] Transform[] _foodParents;
-    [SerializeField] Transform[] _foodTargetNominees;
-    [SerializeField] Transform _hungerTaget;
-    [SerializeField] float _originalSatiety;
+    [SerializeField] Transform[] _foodTargetNominees; //T
+    [SerializeField] Transform _foodTaget; //T
+    [SerializeField] float _originalSatiety; //T
     [SerializeField] float _satiety; // 포만감. 처음 포만감은 최대값이라고 가정
-    [SerializeField] byte _hungerState;
-
-    float _originalSpeed; // 기존 동물 속도 저장용
+    [SerializeField] byte _hungerState; //T
 
     //CognitiveManager values
-    STATE _cognitiveState = STATE.NORMAL;
+    STATE _cognitiveState = STATE.NORMAL; //T
     [SerializeField] Vector3 _cognitiveTarget; //Serialized for test
 
     // Start is called before the first frame update
@@ -72,11 +89,13 @@ public class Animals : MonoBehaviour
     {
         ManageMoveSpeed();
         ManageGimmicks();
+        StaminaManager();
         switch (AnimalManager._frameCounter)
         {
             case 0:
                 {
                     CognitiveManager(_levelOfSize);
+                    
                     break;
                 }
             case 1:
@@ -86,6 +105,7 @@ public class Animals : MonoBehaviour
                 }
             case 2:
                 {
+                    
                     break;
                 }
             case 3:
@@ -97,12 +117,29 @@ public class Animals : MonoBehaviour
                     break;
                 }
         }
-        _agent.SetDestination(_finalTarget);
+        MasterManager();
+        Move();
     }
 
+    //Heavy
     void CognitiveManager(byte animalSize)
     {
         ++animalSize;
+        
+        _cognitiveTarget = AnimalManager.CrudeFlee(transform.position, _cognitiveDistance, animalSize) * _agent.speed * 10;
+        if (_cognitiveTarget == Vector3.zero)
+        {
+            //Search for Chase target
+
+            //If there isn't any, set state to normal
+            _cognitiveState = STATE.NORMAL;
+            _cognitiveTarget = transform.position;
+        }
+        else
+        {
+            _cognitiveState = STATE.RUN;
+            _cognitiveTarget += transform.position;
+        }
         /*
         List<Vector3> SensePredator()
         {
@@ -112,18 +149,10 @@ public class Animals : MonoBehaviour
             return result;
         }
         */
-        _cognitiveTarget = AnimalManager.CrudeFlee(transform.position, _cognitiveDistance, animalSize) * _agent.speed * 10;
-        if (_cognitiveTarget == Vector3.zero)
-        {
-            _cognitiveState = STATE.NORMAL;
-        }
-        else
-        {
-            _cognitiveState = STATE.RUN;
-        }
         //Debug.Log(AnimalManager.Search(transform.position, _cognitiveDistance, animalSize).Count);
     }
 
+    //Heavy
     void HungerManager()
     {
         void SetTarget()
@@ -136,14 +165,14 @@ public class Animals : MonoBehaviour
                 if (sqrDistance > 0 && (sqrDistance < resSqrDist || resSqrDist == 0))
                 {
                     resSqrDist = sqrDistance;
-                    _hungerTaget = nominee;
+                    _foodTaget = nominee;
                 }
             }
         }
         if (_satiety < _originalSatiety * 0.125f)
         {
             _hungerState = 0;
-            if(_hungerTaget is null)
+            if(_foodTaget is null)
             {
                 SetTarget();
             }
@@ -151,7 +180,7 @@ public class Animals : MonoBehaviour
         else if(_satiety < _originalSatiety * 0.125f * 7)
         {
             _hungerState = 1;
-            if (_hungerTaget is null)
+            if (_foodTaget is null)
             {
                 SetTarget();
             }
@@ -162,6 +191,58 @@ public class Animals : MonoBehaviour
         }
     }
 
+    //Very light
+    void StaminaManager()
+    {
+        if(_animalHP <= 0)
+        {
+            DropItems(_dropsWhenDead);
+            Destroy(gameObject);
+        }
+    }
+
+    //light
+    void MasterManager()
+    {
+        switch (_cognitiveState)
+        {
+            case STATE.NORMAL:
+                {
+                    if(_hungerState < 2)
+                    {
+                        _finalTarget = _foodTaget.position;
+                    }
+                    else
+                    {
+                        _finalTarget = _cognitiveTarget;
+                    }
+                    _finalState = _cognitiveState;
+                    break;
+                }
+            case STATE.CHASE:
+                {
+                    if (_hungerState == 0)
+                    {
+                        _finalTarget = _foodTaget.position;
+                        _finalState = STATE.NORMAL;
+                    }
+                    else
+                    {
+                        _finalTarget = _cognitiveTarget;
+                        _finalState = _cognitiveState;
+                    }
+                    break;
+                }
+            case STATE.RUN:
+                {
+                    _finalTarget = _cognitiveTarget;
+                    _finalState = _cognitiveState;
+                    break;
+                }
+        }
+    }
+
+    //light
     void ManageGimmicks()
     {
         foreach (Gimmick gimmick in _gimmicks)
@@ -170,6 +251,7 @@ public class Animals : MonoBehaviour
         }
     }
 
+    //light
     float StateMoveSpeedManager(STATE finalState) // 상태에 따른 이동속도 조절
     {
         // chase run normal hungry
@@ -203,6 +285,19 @@ public class Animals : MonoBehaviour
             StateMoveSpeedManager(_finalState);
     }
 
+    void DropItems(Drop[] drops)
+    {
+        foreach(Drop item in drops)
+        {
+            item.InstantiateDrop(transform.position);
+        }
+    }
+
+    void Move()
+    {
+        _agent.SetDestination(_finalTarget);
+    }
+
     //Public Methods=======================================================================================================================
     public void DecreaseSatiety(float amount)
     {
@@ -212,5 +307,10 @@ public class Animals : MonoBehaviour
     public float GetSatiety()
     {
         return _satiety;
+    }
+
+    public float GetSpeed()
+    {
+        return _agent.speed;
     }
 }
