05315100724b0f5110e9aef42f30610ace9c5dfb
phal5 <77428425+phal5@users.noreply.github.com>
2023-08-23 13:54:09 +0900

    maintenance

--- Assets/XSBD/Prefabs/Animals/AnimalManager.cs
+++ Assets/XSBD/Prefabs/Animals/AnimalManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class AnimalManager : MonoBehaviour
 {
