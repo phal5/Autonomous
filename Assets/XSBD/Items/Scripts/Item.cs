4dbab088a4f57521946719af4e36def8b90ab9ca
cod3builder <dreambuilder255@gmail.com>
2023-09-05 15:53:37 +0900

    GetItemData()

--- Assets/XSBD/Items/Scripts/Item.cs
+++ Assets/XSBD/Items/Scripts/Item.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    Transform _transform;
+    byte _stacksize;
+    bool _isStackable;
 
+    public void GetItemData(ref GameObject itemtype, ref Transform transform, ref byte stacksize, ref bool isStackable)
+    {
+        itemtype = gameObject;
+        transform = _transform;
+        stacksize = _stacksize;
+        isStackable = _isStackable;
+    }
 }
