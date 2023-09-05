d2da93e6195ef575c7b04f2672ff465dcc19b898
cod3builder <dreambuilder255@gmail.com>
2023-09-05 17:23:01 +0900

    Item클래스 제작

--- Assets/XSBD/Items/Scripts/Item.cs
+++ Assets/XSBD/Items/Scripts/Item.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    Transform _transform;
-    byte _stacksize;
+    Transform _parent;
+    byte _quantity;
     bool _isStackable;
 
-    public void GetItemData(ref GameObject itemtype, ref Transform transform, ref byte stacksize, ref bool isStackable)
+    public void GetItemData(ref GameObject itemtype, ref Transform parent, ref byte stacksize, ref bool isStackable)
     {
         itemtype = gameObject;
-        transform = _transform;
-        stacksize = _stacksize;
+        parent = _parent;
+        stacksize = _quantity;
         isStackable = _isStackable;
+    }
+
+    public GameObject GetItem()
+    {
+        return gameObject;
+    }
+    public Transform GetParent()
+    {
+        return _parent;
+    }
+    public byte GetQuantity()
+    {
+        return _quantity;
+    }
+    public bool GetStackable()
+    {
+        return _isStackable;
     }
 }
