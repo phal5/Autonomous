9b0034e87a9f361d8ee31d6f4e22d5cde7e4257e
phal5 <77428425+phal5@users.noreply.github.com>
2023-09-24 01:57:00 +0900

    Update FenceInstaller.cs

--- Assets/XSBD/Program/MinSu/Prefab/Structure/FenceInstaller.cs
+++ Assets/XSBD/Program/MinSu/Prefab/Structure/FenceInstaller.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class FenceInstaller : MonoBehaviour
 {
+    bool _check;
+    float _angle = -120;
+    [SerializeField] Transform _rotator;
+    [SerializeField] Transform _installer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,15 @@ public class FenceInstaller : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Rotate(Transform rotator, ref float degree)
+    {
+        
+    }
+
+    void Install()
+    {
+
     }
 }
